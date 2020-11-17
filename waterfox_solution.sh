b58328d42d6d92021e64a9443bf20b457c09daa2
33Spot <50510349+33Spot@users.noreply.github.com>
2020-11-17 19:15:48 +0100

    Add files via upload

--- waterfox_solution.sh
+++ waterfox_solution.sh
@@ -1,4 +1,5 @@
 
+
 #Amanita.esp B63D92D1
 #Fablewind 1.37v.esp 872639D7
 
@@ -8,7 +9,9 @@ wget $link -O $HOME/waterfox.txt
 file=$HOME/waterfox.txt
 filo=$HOME/waterfox_i.txt
 sed -i -e '/<file\ name=/!d' -e 's/<file\ name=\"//g' -e 's/.glibc2.*//g' -e 's/ //g' $file
+
 wersja=$(cat $file)
+
 if [ -f $filo ];
 then wersja_o=$(cat $filo) 
 else wersja_o= 
@@ -18,12 +21,16 @@ fi
 		Day=`date +%d`
 		UpdateDay=17
 
-		if [ $UpdateDay==$Day ] && [ $wersja_o==$wersja ]; then
+
+
+		if [ $UpdateDay==$Day ] && [ $wersja_o==$wersja ] && [ -f $HOME/waterfox-classic-latest-x86_64.AppImage ]; then
 		chmod +x $HOME/waterfox-classic-latest-x86_64.AppImage
 		cd $HOME
 		$HOME/waterfox-classic-latest-x86_64.AppImage
+		exit 0
 		fi
-		if [ $UpdateDay==$Day ] && !([ $wersja_o==$wersja ]); then
+
+		if [ $UpdateDay==$Day ] && ([ $wersja_o!=$wersja ]); then
 #		notify-send "AKTUALIZUJE WATERFOXA PROSZE POCZEKAJ CHWILE"
 
 zenity \
@@ -40,11 +47,16 @@ zenity \
 		then wersja_o=$(cat $filo) 
 		else wersja_o= 
 		fi
+		if [ -f $file ];
+		then rm -f $file
+		fi
 		chmod +x $HOME/waterfox-classic-latest-x86_64.AppImage
 		cd $HOME
 		$HOME/waterfox-classic-latest-x86_64.AppImage
+		exit 0
 		fi
-		if !([ -f $HOME/waterfox-classic-latest-x86_64.AppImage ]);	
+
+		if [ $UpdateDay==$Day ] && [ $wersja_o==$wersja ] && [ ! -f $HOME/waterfox-classic-latest-x86_64.AppImage ];then
 		wget https://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/AppImage/waterfox-classic-latest-x86_64.AppImage -O $HOME/waterfox-classic-latest-x86_64.AppImage
 		cp -f $file $filo
 		rm -f $file
@@ -52,9 +64,13 @@ zenity \
 		then wersja_o=$(cat $filo) 
 		else wersja_o= 
 		fi
+		if [ -f $file ];
+		then rm -f $file
+		fi
 		#touch $HOME/$wersja$rozszerzenie
 		chmod +x $HOME/waterfox-classic-latest-x86_64.AppImage
 		cd $HOME
 		$HOME/waterfox-classic-latest-x86_64.AppImage]
+		exit 0
 		fi
 		
