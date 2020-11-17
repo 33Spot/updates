e93aa80172bc80366d5f9ded07b4c8efa5beace8
33Spot <50510349+33Spot@users.noreply.github.com>
2020-11-17 17:21:50 +0100

    Add files via upload

--- waterfox_solution.sh
+++ waterfox_solution.sh
@@ -5,35 +5,54 @@
 #!/bin/bash
 link=https://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/AppImage/waterfox-classic-latest-x86_64.AppImage.meta4
 wget $link -O $HOME/waterfox.txt
-sed -e '/<file\ name=/!d' -e 's/<file\ name=\"//g' -e 's/.glibc2.*//g' ./waterfox.txt
-pause
+file=$HOME/waterfox.txt
+filo=$HOME/waterfox_i.txt
+sed -i -e '/<file\ name=/!d' -e 's/<file\ name=\"//g' -e 's/.glibc2.*//g' -e 's/ //g' $file
+wersja=$(cat $file)
+if [ -f $filo ];
+then wersja_o=$(cat $filo) 
+else wersja_o= 
+fi
 
 
 		Day=`date +%d`
 		UpdateDay=17
 
-		if [ $UpdateDay==$Day ] && [ -f $HOME/$wersja$rozszerzenie ]; then
+		if [ $UpdateDay==$Day ] && [ $wersja_o==$wersja ]; then
 		chmod +x $HOME/waterfox-classic-latest-x86_64.AppImage
 		cd $HOME
 		$HOME/waterfox-classic-latest-x86_64.AppImage
 		fi
-		if [ $UpdateDay==$Day ] && !([ -f $HOME/$wersja$rozszerzenie ]); then
+		if [ $UpdateDay==$Day ] && !([ $wersja_o==$wersja ]); then
 #		notify-send "AKTUALIZUJE WATERFOXA PROSZE POCZEKAJ CHWILE"
+
 zenity \
 --info \
---text="<span size=\"xx-large\">Aktualizuję Waterfoxa</span>\n\nBardzo proszę poczekaj chwilę i nie otwieraj narazie <b>WATERFOXA</b>." \
+--text="<span size=\"xx-large\">Aktualizuję Waterfoxa</span>\n\nBardzo proszę kliknij OK i poczekaj chwilę i nie otwieraj narazie <b>WATERFOXA</b>." \
 --title="Coffee time" \
---ok-label="DOBRA"
+--ok-label="Ok"
+
 		rm -f $HOME/waterfox-classic-latest-x86_64.AppImage
 		wget https://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/AppImage/waterfox-classic-latest-x86_64.AppImage -O $HOME/waterfox-classic-latest-x86_64.AppImage
-		echo "">$HOME/$wersja$rozszerzenie
+		cp -f $file $filo
+		rm -f $file
+		if [ -f $filo ];
+		then wersja_o=$(cat $filo) 
+		else wersja_o= 
+		fi
 		chmod +x $HOME/waterfox-classic-latest-x86_64.AppImage
 		cd $HOME
 		$HOME/waterfox-classic-latest-x86_64.AppImage
 		fi
 		if !([ -f $HOME/waterfox-classic-latest-x86_64.AppImage ]);	
 		wget https://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/AppImage/waterfox-classic-latest-x86_64.AppImage -O $HOME/waterfox-classic-latest-x86_64.AppImage
-		echo "">$HOME/$wersja$rozszerzenie
+		cp -f $file $filo
+		rm -f $file
+		if [ -f $filo ];
+		then wersja_o=$(cat $filo) 
+		else wersja_o= 
+		fi
+		#touch $HOME/$wersja$rozszerzenie
 		chmod +x $HOME/waterfox-classic-latest-x86_64.AppImage
 		cd $HOME
 		$HOME/waterfox-classic-latest-x86_64.AppImage]
