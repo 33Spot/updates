9274ab9b0c09467fba14b2787d45ac1fd134965b
33Spot <50510349+33Spot@users.noreply.github.com>
2020-11-18 23:22:11 +0100

    Add files via upload

--- waterfox_solution.sh
+++ waterfox_solution.sh
@@ -4,33 +4,30 @@
 #Fablewind 1.37v.esp 872639D7
 
 #!/bin/bash
+Day=`date +%d`
+UpdateDay=17
 link=https://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/AppImage/waterfox-classic-latest-x86_64.AppImage.meta4
-wget $link -O $HOME/waterfox.txt
 file=$HOME/waterfox.txt
 filo=$HOME/waterfox_i.txt
+
+if [ $UpdateDay = $Day ]; then
+wget $link -O $HOME/waterfox.txt
 sed -i -e '/<file\ name=/!d' -e 's/<file\ name=\"//g' -e 's/.glibc2.*//g' -e 's/ //g' $file
-
 wersja=$(cat $file)
-
 if [ -f $filo ];
 then wersja_o=$(cat $filo) 
 else wersja_o= 
 fi
+fi
 
-
-		Day=`date +%d`
-		UpdateDay=17
-
-
-
-		if [ $UpdateDay==$Day ] && [ $wersja_o==$wersja ] && [ -f $HOME/waterfox-classic-latest-x86_64.AppImage ]; then
+		if [ $wersja_o = $wersja ] && [ -f $HOME/waterfox-classic-latest-x86_64.AppImage ]; then
 		chmod +x $HOME/waterfox-classic-latest-x86_64.AppImage
 		cd $HOME
 		$HOME/waterfox-classic-latest-x86_64.AppImage
 		exit 0
 		fi
 
-		if [ $UpdateDay==$Day ] && ([ $wersja_o!=$wersja ]); then
+		if [ $UpdateDay = $Day ] && ([ $wersja_o != $wersja ]); then
 #		notify-send "AKTUALIZUJE WATERFOXA PROSZE POCZEKAJ CHWILE"
 
 zenity \
@@ -56,7 +53,7 @@ zenity \
 		exit 0
 		fi
 
-		if [ $UpdateDay==$Day ] && [ $wersja_o==$wersja ] && [ ! -f $HOME/waterfox-classic-latest-x86_64.AppImage ];then
+		if [ $UpdateDay = $Day ] && [ $wersja_o = $wersja ] && [ ! -f $HOME/waterfox-classic-latest-x86_64.AppImage ];then
 		wget https://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/AppImage/waterfox-classic-latest-x86_64.AppImage -O $HOME/waterfox-classic-latest-x86_64.AppImage
 		cp -f $file $filo
 		rm -f $file
