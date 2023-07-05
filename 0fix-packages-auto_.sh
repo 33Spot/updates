34493d8cedc69b006bfbe38bbdf2e9f2158af814
33Spot <50510349+33Spot@users.noreply.github.com>
2023-07-05 23:32:49 +0200

    Update 0fix-packages-auto_.sh

--- 0fix-packages-auto_.sh
+++ 0fix-packages-auto_.sh
@@ -171,9 +171,11 @@ cat /etc/apt/sources.list | perl -ne '$H{$_}++ or print' > /tmp/sources.list && 
 sudo snap refresh
 flatpak uninstall --unused 2>/dev/null
 #pip list --outdated
+pip3 cache purge
+pip3 install --upgrade pip setuptools wheel
 python3 -m pip install --upgrade pip
 pip3 list --outdated --format=freeze 2>/dev/null | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U 2>/dev/null
-pip cache purge
+
 
 
 
