c5c7df807f4a186c8b2fee1d8740312b067f049e
33Spot <50510349+33Spot@users.noreply.github.com>
2023-07-08 22:01:38 +0200

    Add files via upload

---
+++ xbindkeys.sh
@@ -0,0 +1,33 @@
+sudo apt-get install xbindkeys
+#xbindkeys --defaults > ~/.xbindkeysrc
+#lapanie sskrotow klawiaturowych
+#xbindkeys -k
+cp ./xbindkeysrc ~/.xbindkeysrc
+xbindkeys --file ~/.xbindkeysrc
+# gnome-session-properties
+
+
+#       Available command line options are as follows:
+#       -d, --defaults
+#              Print a default rc file
+#       -f, --file
+#              Use an alternative rc file
+#       -h, --help
+#              Short help on options plus version/author info.
+#       -X, --display
+#              Set X display to use
+#       -v, --verbose
+#              More information on xbindkeys when it run
+#       -k, --key
+#              Identify one key pressed (useful to fill the configuration file)
+#       -mk, --multikey
+#              Identify multi key pressed (useful to fill the configuration file)
+#       -g, --geometry
+#              size and position of window with -k|-mk option
+#       -n, --nodaemon
+#              don't start as daemon
+#       If guile support have not been disabled:
+#       -dg, --defaults-guile
+#              Print a default guile configuration file
+#       -fg, --file-guile
+#              Use an alternative guile configuration file
