2d224fd8895e69289c662b82eee40ec81bf0def1
33Spot <50510349+33Spot@users.noreply.github.com>
2023-07-09 23:10:57 +0200

    Add files via upload

---
+++ snap.sh
@@ -0,0 +1,27 @@
+sudo mv /etc/apt/preferences.d/nosnap.pref /etc/apt/preferences.d/nosnap.preff
+sudo apt update
+sudo apt install snapd -y
+#sudo snap install gimp
+#sudo snap install solitaire
+#sudo snap install ghex-udt
+#sudo snap install cloudtag
+#sudo snap install mjpg-streamer
+#sudo snap install foobar2000
+#sudo snap install ultimate-media-downloader2
+#sudo snap install snap-store
+
+#sudo snap install wonderwall
+#sudo snap connect wonderwall:hardware-observe
+
+#Downloaded wallpaper location info
+#Currently snap has set download location under 
+
+#~/snap/wonderwall/current/.local/share/ktechpit/WonderWall/download
+
+#For people who used debian package can find downloaded wallpapers here: 
+
+#~/.local/share/ktechpit/WonderWall/download
+
+#anime kitty 94
+#101
+#127
