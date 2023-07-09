2b83102d351452bc61bad094d4d56801a2b03416
33Spot <50510349+33Spot@users.noreply.github.com>
2023-07-09 12:46:20 +0200

    Add files via upload

--- 0Freetube_cleaner.sh
+++ 0Freetube_cleaner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 OIFS="$IFS"
 IFS=$'\n'
+
 # Declare an array of string with type
 declare -a foldery_usun=("blob_storage" "Cache" "Code Cache" "Crashpad" "dashFiles" "DawnCache" "Dictionaries" "GPUCache" "Local Storage" "Session Storage" "storyboards" "WebStorage")
-
 
 # Iterate the string array using for loop
 for val in ${foldery_usun[@]}; do
@@ -13,7 +13,6 @@ done
 
 # Declare an array of string with type
 declare -a pliki_usun=("TransportSecurity" "Cookies-journal" "Network Persistent State" "Cookies")
-
 
 # Iterate the string array using for loop
 for val in ${pliki_usun[@]}; do
