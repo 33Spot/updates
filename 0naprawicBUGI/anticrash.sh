4c8acd6da068feba6499845a4793968d0420ec95
33Spot <50510349+33Spot@users.noreply.github.com>
2023-07-20 19:33:26 +0200

    Add files via upload

---
+++ 0naprawicBUGI/anticrash.sh
@@ -0,0 +1,3 @@
+sudo apt install libpng-dev
+sudo apt install libcairo2-dev
+sudo apt install libcjs-dev libmozjs*
---
+++ 0naprawicBUGI/fsck-on.sh
@@ -0,0 +1,3 @@
+sudo sed "/GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash fsck.mode=force fsck.repair=yes\"" /etc/default/grub -i
+sudo update-grub
+#shutdown -rF now
