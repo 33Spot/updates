ca51d4f717613eb214933aefa0e3d5ddd8a43166
33Spot <50510349+33Spot@users.noreply.github.com>
2023-08-06 08:35:41 +0200

    Add files via upload

--- 0naprawicBUGI/fsck-on.sh
+++ 0naprawicBUGI/fsck-on.sh
@@ -1,3 +1,4 @@
-sudo sed "/GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash fsck.mode=force fsck.repair=yes\"" /etc/default/grub -i
+#sudo sed "/GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash fsck.mode=force fsck.repair=yes\"" /etc/default/grub -i
+sudo sed "/GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash fsck.mode=auto fsck.repair=yes\"" /etc/default/grub -i
 sudo update-grub
 #shutdown -rF now
