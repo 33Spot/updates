f7fd1648b13bc7d229369706ed8d836c41cfb6ce
33Spot <50510349+33Spot@users.noreply.github.com>
2023-09-28 00:03:30 +0200

    Add files via upload

--- update_SOFT.sh
+++ update_SOFT.sh
@@ -1,29 +1,69 @@
 
+#./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github.com/FreeTubeApp/FreeTube/actions/workflows/build.yml | grep "FreeTube/actions/runs/" | grep -v "workflow" | head -n 1
+
+
+#https://github.com/FreeTubeApp/FreeTube/actions/workflows/build.yml
+
+
+#https://github.com/FreeTubeApp/FreeTube/actions/runs/5789409466
+
+#https://github.com/FreeTubeApp/FreeTube/actions/runs/5789409466/job/15690402588
+
+#./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github.com/FreeTubeApp/FreeTube/actions/runs/5789409466 | grep "job" | head -n 1
+
+
+#curl -H "Accept: application/vnd.github+json" -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" -L -o firmware.zip "https://api.github.com/repos/ExpressLRS/ExpressLRS/actions/artifacts/308108746/zip" 
+#curl -H "Accept: application/vnd.github+json" -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" -L -o firmware.zip "https://github.com/FreeTubeApp/FreeTube/suites/14895239864/artifacts/848605744" 
 
 
 
-password=
+
+#/FreeTubeApp/FreeTube/suites/14895239864/artifacts/848605747
+
+
+
+
+
+#curl -L \
+#  -H "Accept: application/vnd.github+json" \
+#  -H "Authorization: Bearer <YOUR-TOKEN>" \
+ # -H "X-GitHub-Api-Version: 2022-11-28" \
+ # https://api.github.com/repos/OWNER/REPO/actions/artifacts/ARTIFACT_ID
+
+
+#  curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer <YOUR-TOKEN>" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/FreeTubeApp/FreeTube/actions/artifacts/848605747
+
+
+
+
+password=qaz123
 
 gdebi=dpkg\ \-\-force-all\ \-i
 
 
 #./xidel -e '(//@href, //@src)/resolve-uri(.)' https://www.xnview.com/en/xnviewmp/
 
-url_p1=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://download.anydesk.com/linux/ | sed '/\.sum/d' | grep "_amd64.deb" | grep "linux/anydesk" | tail -n 1)
-echo ANYDESK: $url_p1
+#url_p1=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://download.anydesk.com/linux/ | sed '/\.sum/d' | grep "_amd64.deb" | grep "linux/anydesk" | tail -n 1)
+#echo ANYDESK: $url_p1
 url_p2=$(curl -s -L http://www.xnview.com/en/xnviewmp/|sed -n -E '/Version/{s/.*Version ([\.0-9]+).*/\1/;s/\.//;p;q;}' | sed 's/\.//g')
 echo XNVIEW: $url_p2
-url_p3=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://www.torproject.org/download/alpha/ | grep "linux64" | sed '/\.asc/d')
+url_p3=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://www.torproject.org/download/alpha/ | grep "\-linux\-x86_64\-" | sed '/\.asc/d')
 echo TOR: $url_p3
 
-url_p4=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github.com/FreeTubeApp/FreeTube-Nightly/releases/ | grep "releases/tag"  | grep "FTN" | head -n 1)
+url_p4=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github.com/FreeTubeApp/FreeTube/releases/ | grep "releases/tag" | head -n 1)
 url_p4b=$(echo $url_p4 | sed 's/tag/expanded_assets/g')
 url_p4c=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' $url_p4b | grep "_amd64.deb" | tail -n 1)
 echo FREETUBE: $url_p4c
 
+url_p4=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github.com/FreeTubeApp/FreeTube-Nightly/releases/ | grep "releases/tag"  | grep "FTN" | head -n 1)
+url_p4b=$(echo $url_p4 | sed 's/tag/expanded_assets/g')
+url_p4c=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' $url_p4b | grep "_amd64.deb" | tail -n 1)
+echo FREETUBE-NIGHTLY: $url_p4c
+
 url_p=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github\.com/ferdium/ferdium-app/releases | grep "releases/expanded" | head -n 1)
 url_5=https://github.com$(wget -qO- $url_p | grep "amd64.deb" | sed -e "/span/d" -e "/ferdium/,/deb/p" | sed -n '/"/!{/\n/{P;b}};s/"/\n/g;D' | head -n 1)
 echo FERDIUM: $url_5
+
 url_p5=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github\.com/subhra74/xdm/releases | grep "releases/expanded" | head -n 1)
 url_6=https://github.com$(wget -qO- $url_p5 | grep "amd64.deb" | sed -e "/span/d" -e "/xdman/,/deb/p" | sed -n '/"/!{/\n/{P;b}};s/"/\n/g;D' | head -n 1)
 echo XDM: $url_6
@@ -105,7 +145,37 @@ fi
 
 
 
-url=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://download.anydesk.com/linux/ | sed '/\.sum/d' | grep "_amd64.deb" | grep "linux/anydesk" | tail -n 1)
+#url=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://download.anydesk.com/linux/ | sed '/\.sum/d' | grep "_amd64.deb" | grep "linux/anydesk" | tail -n 1)
+#debpackage=${url##*/}
+#package=./$debpackage
+#if [ ! -f $package ]; then
+#(
+#wget --no-check-certificate $url
+#echo $password | sudo -S $gdebi $package
+#)
+#fi
+#
+#if [ -f $package ]; then
+#(
+#rm -f $package wget-log
+#)
+#fi
+
+
+#url_p=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github\.com/FreeTubeApp/FreeTube/releases | grep "releases/expanded" | head -n 1)
+#echo $url_p
+#echo press enter to download...
+#read r
+
+
+
+
+
+
+
+url_p4=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github.com/FreeTubeApp/FreeTube/releases/ | grep "releases/tag" | head -n 1)
+url_p4b=$(echo $url_p4 | sed 's/tag/expanded_assets/g')
+url=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' $url_p4b | grep "_amd64.deb" | tail -n 1)
 debpackage=${url##*/}
 package=./$debpackage
 if [ ! -f $package ]; then
@@ -122,10 +192,11 @@ rm -f $package wget-log
 fi
 
 
-#url_p=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github\.com/FreeTubeApp/FreeTube/releases | grep "releases/expanded" | head -n 1)
-#echo $url_p
-#echo press enter to download...
-#read r
+
+
+
+
+
 
 #url=https://github.com$(wget -qO- 'https://github.com/FreeTubeApp/FreeTube/releases/latest' | grep "amd64.deb" | sed -e "/span/d" -e "/FreetubeApp/,/deb/p" | sed -n '/"/!{/\n/{P;b}};s/"/\n/g;D' | head -n 1)
 url_p4=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github.com/FreeTubeApp/FreeTube-Nightly/releases/ | grep "releases/tag"  | grep "FTN" | head -n 1)
@@ -135,8 +206,9 @@ debpackage=${url##*/}
 package=./$debpackage
 if [ ! -f $package ]; then
 (
-wget --no-check-certificate $url
-echo $password | sudo -S $gdebi $package
+	echo nic tu nie ma....
+#wget --no-check-certificate $url
+#echo $password | sudo -S $gdebi $package
 )
 fi
 
