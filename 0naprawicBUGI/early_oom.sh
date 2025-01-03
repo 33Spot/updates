a35acb6f4833f15134c37dc9e327cc50f686697c
33Spot <50510349+33Spot@users.noreply.github.com>
2025-01-03 12:50:15 +0100

    Add files via upload

--- 0naprawicBUGI/early_oom.sh
+++ 0naprawicBUGI/early_oom.sh
@@ -1,2 +1,23 @@
 sudo apt-get install earlyoom
-sudo apt-get install oomd
+#sudo apt-get install oomd
+
+#sudo systemctl enable --now oomd.service
+#sudo systemctl enable --now oomd.service
+sudo systemctl unmask earlyoom.service
+sudo systemctl enable --now earlyoom
+sudo systemctl start earlyoom
+systemctl status earlyoom
+
+#sudo systemctl start oomd
+#systemctl status oomd
+
+
+
+
+
+sudo sh -c "echo '1' > /proc/sys/vm/oom_kill_allocating_task"
+cat /proc/sys/vm/oom_kill_allocating_task
+sudo sed "/kernel.sysrq=/c\kernel.sysrq=1" /etc/sysctl.conf -i
+sudo sysctl -p
+
+
