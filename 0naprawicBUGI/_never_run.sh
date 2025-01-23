51e6522c20f78bb8e707ad83851ce12cb49ca4ae
33Spot <50510349+33Spot@users.noreply.github.com>
2025-01-23 19:52:11 +0100

    Add files via upload

---
+++ 0naprawicBUGI/_never_run.sh
@@ -0,0 +1,6 @@
+#echo how to broke your system
+#sudo chmod 0755 $(which pkexec)
+#echo repairing...
+#sudo chmod 0777 $(which pkexec)
+#sudo chmod u+s $(which pkexec)
+
--- 0naprawicBUGI/systemtap.sh
+++ 0naprawicBUGI/systemtap.sh
@@ -1,6 +1,2 @@
 sudo apt-get -y install systemtap
-#echo how to broke your system
-#sudo chmod 0755 $(which pkexec)
-#echo repairing...
-#sudo chmod 0777 $(which pkexec)
-#sudo chmod u+s $(which pkexec)
+
