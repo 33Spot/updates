357a7dffe52592256dcbcd93db32355510ceb8d9
33Spot <50510349+33Spot@users.noreply.github.com>
2025-02-11 22:59:44 +0100

    Delete 0AMinstall.sh

--- 0AMinstall.sh
+++
@@ -1,6 +0,0 @@
-wget -q https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER
-chmod a+x ./AM-INSTALLER
-./AM-INSTALLER
-am --disable-notifications
-#https://portable-linux-apps.github.io
-read r
