198c85938f8e5be0f67155992be1550fae86465c
33Spot <50510349+33Spot@users.noreply.github.com>
2025-11-25 17:47:34 +0100

    Update install.sh

--- unpacker/install.sh
+++ unpacker/install.sh
@@ -3,8 +3,8 @@
 file=hl_csgo_10.arc
 link=https://mega.nz/file/oMZw2bBR#4hpni1Sko2VPWzuR09iDILKxQ14Th7o9VQ7H95qmNHQ
 
-wget https://github.com/33Spot/updates/raw/refs/heads/master/unpacker/deps_arch.sh && chmod +x deps_arch.sh && clear
-wget https://github.com/33Spot/updates/raw/refs/heads/master/unpacker/deps_debian.sh && chmod +x deps_debian.sh && clear
+wget https://github.com/33Spot/updates/raw/refs/heads/master/unpacker/deps_arch.sh && chmod +x deps_arch.sh && clear && ./deps_arch.sh
+wget https://github.com/33Spot/updates/raw/refs/heads/master/unpacker/deps_debian.sh && chmod +x deps_debian.sh && clear && ./deps_debian.sh
 
 wget https://github.com/33Spot/updates/raw/refs/heads/master/unpacker/megatools && chmod +x megatools && clear
 wget https://github.com/33Spot/updates/raw/refs/heads/master/unpacker/fa && chmod +x fa && clear
