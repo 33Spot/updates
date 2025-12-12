abc19f8e85d21977d1417969b81b4b9d969652c9
33Spot <50510349+33Spot@users.noreply.github.com>
2025-12-12 23:12:23 +0100

    Add files via upload

---
+++ AM/install.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+wget https://github.com/33Spot/updates/raw/refs/heads/master/AM/0AM%20-%20install.sh && chmod +x '0AM - install.sh' && ./'0AM - install.sh'
+wget https://github.com/33Spot/updates/raw/refs/heads/master/AM/AM%20-%20pathfix.sh && chmod +x 'AM - pathfix.sh' && ./'AM - pathfix.sh'
+wget https://github.com/33Spot/updates/raw/refs/heads/master/AM/am%20addpath.sh && chmod +x 'am addpath.sh' && ./'am addpath.sh'
+
+
+
+
+
