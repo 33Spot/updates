699602d0d8639cef715721cdfe9dd2d18aece1b5
33Spot <50510349+33Spot@users.noreply.github.com>
2025-12-12 23:13:30 +0100

    Update install.sh

--- AM/install.sh
+++ AM/install.sh
@@ -1,4 +1,7 @@
 #!/bin/env bash
+
+# wget https://github.com/33Spot/updates/raw/refs/heads/master/AM/install.sh && chmod +x install.sh && ./install.sh
+
 wget https://github.com/33Spot/updates/raw/refs/heads/master/AM/0AM%20-%20install.sh && chmod +x '0AM - install.sh' && ./'0AM - install.sh'
 wget https://github.com/33Spot/updates/raw/refs/heads/master/AM/AM%20-%20pathfix.sh && chmod +x 'AM - pathfix.sh' && ./'AM - pathfix.sh'
 wget https://github.com/33Spot/updates/raw/refs/heads/master/AM/am%20addpath.sh && chmod +x 'am addpath.sh' && ./'am addpath.sh'
