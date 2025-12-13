4c3c2e7bfa8b63afb82b47889894db3842a77923
33Spot <50510349+33Spot@users.noreply.github.com>
2025-12-13 21:29:51 +0100

    Update dlg.sh

--- unpacker/dlg.sh
+++ unpacker/dlg.sh
@@ -3,42 +3,8 @@
 wget https://github.com/33Spot/updates/raw/refs/heads/master/unpacker/megatools && chmod +x megatools && clear
 wget https://github.com/33Spot/updates/raw/refs/heads/master/unpacker/fa && chmod +x fa && clear
 
-#./megatools dl https://mega.nz/file/XQgDlJiY#oiMqvv43nBmXUiW0B03iglG_wmcSq1Qdp-x1euirHSw
-#./megatools dl https://mega.nz/file/mEgkCKCA#hHg-mxWyUsoQapb-8EhDe-zdBzkHxIfVJmbP-GZw3K8
-#./megatools dl https://mega.nz/file/XYBE0CBL#ro0l-02WkOQhPY-_8fW8NJTOG00VLYKcABO2ETZOJYc
-#./megatools dl https://mega.nz/file/rN4UxRra#X-0_NGAX9BPqJHXpbZP1nWWlZisnd55_R0HbIw2nh7w
-#./megatools dl https://mega.nz/file/mdBBgCJC#YMGE1EdoYmL7m_F351i_AV_Dg2q4x1kjsN5WXDuTyn4
-#./megatools dl https://mega.nz/file/fdxwUaxD#b9Z_IOax4NpiSXugpR1O54TzZDslgFRUIsAwUhGFh78
-#./megatools dl https://mega.nz/file/yVZVnAJT#225oepg-b5-ARqnIVwcH4TiAez3PkvrtpMrScQRQTMQ
-#./megatools dl https://mega.nz/file/OBoRCC4L#1sFOTFLtbpe56Q8itax8L_Z-Tp9xpl-EJZfKCvOrLJc
-#./megatools dl https://mega.nz/file/zEgVlT6R#TTe2W6cOE9fpAI-AV-zTH1fsO_8vE3HiyjZsM3Yhvpg
-#./megatools dl https://mega.nz/file/HIoS3JRb#AYm3afyybwXKIzXo1q9utG75vdRq8Rk_h0cXhCUEYrg
-#./megatools dl https://mega.nz/file/rB5TGJaB#B9qVtURpgQ8_wjCdeWj3m7Cm7t4sNiAGwXYU3BXgUNs
-#./megatools dl https://mega.nz/file/ucQhFJhY#fyLcllPb0tgq3vEikWoriej5ze2v8REeeGmPm5xszMs
-#./megatools dl https://mega.nz/file/6UxkmSDC#lOGQxipbkctlRHCw1clyYcEe2aCsmTtnK6ti2a1lRwI
-#./megatools dl https://mega.nz/file/acBEkR7T#8UfE3w4rj77u4faNp9zbvzeiZhCM3Gg1DhP9PsfkSHg
-#./megatools dl https://mega.nz/file/aZAWEaBZ#tcgOHUfT4JjaVywAvfmoGoNq1tiD-2F8Hr2jVAmyh2w
-#./megatools dl https://mega.nz/file/DUw20SKA#iRETz4mb8e-t3GbT9Jao4F79JmjkA5m1LVcDZop0Xsw
-#./megatools dl https://mega.nz/file/6UY3gJjZ#JDP16FG8uHvYcmmvJYwZZLYUlEZqzgSJ1qeRjOfnZfY
-#./megatools dl https://mega.nz/file/XEB3jTbT#Xtbo-EOqkDWVVJAMAfnP0cLsgj1nhmbsdRB3w7I5TQk
-#./megatools dl https://mega.nz/file/3IA1hTQA#TrVgRYIcjt9zqKjSg-2PJyndLDWDQmGoZmQ3CQIoJis
-#./megatools dl https://mega.nz/file/TYR2UTzY#piTv6KUNyigqjz3By-o9yOYZYzsWl4MpgopvLxgwBDk
-#./megatools dl https://mega.nz/file/jYpBCAaa#zEfs5-WN_jgs-CwqeWZypxBOtU482yd45ajErZK5o88
-#./megatools dl https://mega.nz/file/OZoUESxI#UM2N9DbiNxyl9gjXln3KsqAMyq9fhFVVBZjfJBYmT08
-#./megatools dl https://mega.nz/file/uYY3GJ5C#Vkn38qA8TQezmsvuj-KpUxNaoIC_fwdRVr6XxEJuS2Y
-#./megatools dl https://mega.nz/file/yIwF3Kya#-wMsD2NpWGkRxyOnODdkOcfr5ez1KwLOXr7KCXI0h1M
-#./megatools dl https://mega.nz/file/HJ4lACwS#6JKEarHkYQlfiiRrtUiXvflfwTz3ALN9IYhzhAVXejg
-#./megatools dl https://mega.nz/file/2F5HVRLB#-hjH-V4g2Rou0RUwILDh4GwSf7mEgV4naWa0Sxx0114
-#./megatools dl https://mega.nz/file/SEgFCQQb#_m6UVl2dPMWi9Aw9JEKCzCofOZYpj5CxA6ix-bfOvFY
-#./megatools dl https://mega.nz/file/XUAUWBBL#bxsUiymdK_dYMu0_9XqTKdux9UoYKb4F9G5ZBa5Yra4
-#./megatools dl https://mega.nz/file/mYJWTa6J#MM_udOLBDrPw9UUkXi22HdJsDQNRUENCijz7EU2gGtE
-#./megatools dl https://mega.nz/file/7A4hHAha#hgw_piOxJW9mLyszNJImgGQ5xRCemhnk648U5jv02oE
-#./megatools dl https://mega.nz/file/XdoXUa5C#Awhk012dAKqXsz48bTm8OSi189RSyp04Y-ZHGv4jOmY
-#./megatools dl https://mega.nz/file/rc5REZDZ#TEUtiyRaauKGcW_shqczGXFBAFvvAANjkyBapHS3sFM
-#./megatools dl https://mega.nz/file/DJowybrQ#lP6R2APNUkqy1V_owLUzvIktrrF-3rDzCHAW3kEVJds
-#./megatools dl https://mega.nz/file/HI5gjLJZ#4pi8gl-GmMSXV-ct1_ieRkCDIEHHSlbUJc-RYm79uwA
-
 ./megatools dl https://mega.nz/file/bQR2CTaS#qqkqZEw22Eo0CP3OQLwadEIPztId-tkTgcZzMZihoBY
+./megatools dl https://mega.nz/file/aIhREY5R#5S-le59SXsDKqp-0x4A3IL_ANT6N5aMV8T5hpfqO3Fs
 ./megatools dl https://mega.nz/file/nYZxiKhK#vmGlPK_eWPuMlxbDKFCcSv0nzhg-HkgvzsF6iKp0ylg
 ./megatools dl https://mega.nz/file/2VBWVZ7C#fpqG-R_W1uZs2HOfS67_AUH9wHFsPB9FJaCbaJxuZhE
 ./megatools dl https://mega.nz/file/eI4hgIhA#RNwX9Ejdh8CF0JdFlB_Hyz_bu84_ziUtjXI7sEj-rBc
@@ -50,8 +16,8 @@ wget https://github.com/33Spot/updates/raw/refs/heads/master/unpacker/fa && chmo
 ./megatools dl https://mega.nz/file/bRYl1QbD#eMpo30xlNoYzfBqKmeapIWjKuqEcWYfGrh_FXJq-A_I
 ./megatools dl https://mega.nz/file/iIRFVB4b#iiNU16g7XBFRmVZWznx7oP86_YBID8SBuWc6sGBMrG4
 ./megatools dl https://mega.nz/file/TFQRmK4S#5ixv54PTkb4608Kj1F-QansEZefP9CW3rfINAOXPhcs
+./megatools dl https://mega.nz/file/6I41WbAa#ySKERrPsdL6O6IYrlUXYM4Mc_Z53__1V22antWgQFCQ
 ./megatools dl https://mega.nz/file/vQIiBIRB#VqNxpMApeTo614qbIBgYH9BgA6N0SwM4RVXVPO1v7Do
-
 
 
 
