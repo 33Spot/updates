37151e846b7565ab43ffe46598a9dc744076dc16
33Spot <50510349+33Spot@users.noreply.github.com>
2026-01-25 15:34:08 +0100

    Update dlg.sh

--- unpacker/dlg.sh
+++ unpacker/dlg.sh
@@ -3,6 +3,8 @@
 wget https://github.com/33Spot/updates/raw/refs/heads/master/unpacker/megatools && chmod +x megatools && clear
 wget https://github.com/33Spot/updates/raw/refs/heads/master/unpacker/fa && chmod +x fa && clear
 
+./megatools dl https://mega.nz/file/SRRUjbaR#mglSyHJZZwvs5B1nfKVLdeXe_406jdRGKxJcAe5ZiKU
+
 ./megatools dl https://mega.nz/file/KBIBWYpZ#97JbM5tM4ZVl0tug30UPaqG2HjlfYSA77d2GNpSrmBE
 ./megatools dl https://mega.nz/file/aAATgKhI#Q_HCc3Z1WfBfF4LSjowKgp5eLIP67q-PFiu51N8oD34
 ./megatools dl https://mega.nz/file/SEY2zRCL#V8JrYsiQa8uc3uWFLbpyB3atv_HCoP-Aws_ZsbgsqBo
